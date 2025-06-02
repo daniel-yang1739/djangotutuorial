803ddff808041169234dba71a6477eb402f8ebbf
daniel-yang1739 <ytyang6690@gmail.com>
2025-06-02 10:44:09 +0800

    add index.html and update index view

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render
+from polls.models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = 'polls/index.html'
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, template, context)
 
 
 def details(request, question_id):
