1bcc89508365ec72eb59d2f711ed75c750892afd
daniel-yang1739 <ytyang6690@gmail.com>
2025-06-02 10:31:29 +0800

    add more views

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from polls.views import index
+from polls.views import details
+from polls.views import results
+from polls.views import vote
 
 urlpatterns = [
     path('', index, name='index'),
+    path("<int:question_id>/", details, name="details"),
+    path("<int:question_id>/results/", results, name="results"),
+    path("<int:question_id>/vote/", vote, name="vote"),
 ]
--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def details(request, question_id):
+    response = f"You're looking at the details of question {question_id}"
+    return HttpResponse(response)
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
