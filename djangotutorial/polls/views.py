14cdb6071e86ff241a9153f79782dcf24d4ba65b
daniel-yang1739 <ytyang6690@gmail.com>
2025-06-04 23:13:24 +0800

    remove redundent import

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,5 +1,4 @@
 from django.db.models import F
-from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
